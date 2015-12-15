a40a5f9d769249e2a026c5aa8988aa54ffbb9288
Jonathan Ross <jonross@alum.mit.edu>
2015-12-14 23:17:19 -0500

    Reverse round args

--- tests/round.py
+++ tests/round.py
@@ -1,21 +1,21 @@
 
 test("""missing round arg 1""", """round""", PS, """""", """
-Missing argument, expected # of places
-""")
-
-test("""bad round arg 1""", """round x""", PS, """""", """
-Expected # of places but got x
-""")
-
-test("""missing round arg 2""", """round 1""", PS, """""", """
 Missing argument, expected column expression
 """)
 
-test("""bad round arg 2""", """round 1 x""", PS, """""", """
+test("""bad round arg 1""", """round x""", PS, """""", """
 Invalid column: x
 """)
 
-test("""round absent column""", """round 1 99""", PS, PSOUT, """""")
+test("""missing round arg 2""", """round 1""", PS, """""", """
+Missing argument, expected # of places
+""")
+
+test("""bad round arg 2""", """round 1 x""", PS, """""", """
+Expected # of places but got x
+""")
+
+test("""round absent column""", """round 99 1""", PS, PSOUT, """""")
 
 test("""round 2 places""", """csv round 2 2""", """
 1.123,1.123,1.123
@@ -25,7 +25,7 @@ test("""round 2 places""", """csv round 2 2""", """
 2.246,2.25,2.246
 """, """""")
 
-test("""round 1 place""", """csv round 1 2""", """
+test("""round 1 place""", """csv round 2 1""", """
 1.123,1.123,1.123
 2.246,2.246,2.246
 """, """
@@ -33,7 +33,7 @@ test("""round 1 place""", """csv round 1 2""", """
 2.246,2.2,2.246
 """, """""")
 
-test("""round -1 places""", """csv round -1 2""", """
+test("""round -1 places""", """csv round 2 -1""", """
 1.123,12.2,1.123
 2.246,16.2,2.246
 """, """
@@ -41,7 +41,7 @@ test("""round -1 places""", """csv round -1 2""", """
 2.246,20.0,2.246
 """, """""")
 
-test("""round all""", """csv round 0 1-""", """
+test("""round all""", """csv round 1- 0""", """
 1.123,12.2,1.123
 2.246,16.2,2.246
 """, """
