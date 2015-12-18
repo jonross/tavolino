850e93fa316025bef71be7f0579901eac0f6770f
Jonathan Ross <jonross@alum.mit.edu>
2015-12-17 22:44:33 -0500

    Support None in deltas

--- tests/delta.py
+++ tests/delta.py
@@ -7,7 +7,7 @@ test("""bad delta arg""", """delta x""", PS, """""", """
 Invalid column: x
 """)
 
-test("""delta""", """delta 1,4""", """
+test("""delta ints""", """delta 1,4""", """
 1 a b 6
 3 c d 3
 6 e f 1
@@ -17,7 +17,17 @@ test("""delta""", """delta 1,4""", """
 6	3	e	f	1	-2
 """, """""")
 
-test("""delta""", """delta 1,4""", """
+test("""delta with null""", """delta 1,4""", """
+1 a b 6
+3 c d
+6 e f 1
+""", """
+1	0	a	b	6	0
+3	2	c	d		0
+6	3	e	f	1	-5
+""", """""")
+
+test("""delta floats""", """delta 1,4""", """
 1.0 a b 6.0
 3.0 c d 3.0
 6.0 e f 1.0
